a0e4d7aafcf4d7d82330a9ad7fa6558b4c7004ae
angiekim05 <favor3213@gmail.com>
2022-11-10 23:59:20 +0900

    DFS

---
+++ Baekjoon/gold3/1520.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def dfs(x,y):
+    if x == n-1 and y == m-1:
+        return 1
+    
+    # 방문한 적이 있다면, 해당 위치에서 출발하는 경우의 수 return
+    if visited[x][y] != -1:
+        return visited[x][y]
+
+    cnt = 0
+    for i in range(4):
+        nx,ny = x+dx[i], y+dy[i]
+        if nx<0 or nx>=n or ny<0 or ny>=m: # 지도 밖이면 pass
+            continue
+        if maps[nx][ny] >= maps[x][y]: # 내리막길이 아니면 pass
+            continue
+        cnt += dfs(nx,ny)
+
+    visited[x][y] = cnt
+    return cnt
+
+
+n,m = map(int,input().split())
+maps = [list(map(int,input().split())) for _ in range(n)]
+dx,dy = (0,0,1,-1),(1,-1,0,0)
+visited = [[-1]*m for _ in range(n)]
+print(dfs(0,0))
