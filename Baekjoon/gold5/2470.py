0d60c4e398c1bfaa1584124dcd19a237437d853e
angiekim05 <favor3213@gmail.com>
2022-12-14 21:24:03 +0900

    정렬 & 두포인터

---
+++ Baekjoon/gold5/2470.py
@@ -0,0 +1,18 @@
+# 정렬한 뒤, 두 포인터를 활용하여 절대값이 최소가 되는 쌍을 구하기
+
+n = int(input())
+a = list(map(int,input().split()))
+a.sort()
+i,j = 0, n-1
+ans = [a[i],a[j]] # 정답 담기
+bf = abs(a[i]+a[j]) # 절대값 비교를 위해 절대값 저장
+while i < j:
+    now = a[i] + a[j] # 현재 값 계산
+    if bf > abs(now): # 절대값 비교해서 현재가 더 작으면 업데이트
+        bf = abs(now)
+        ans = [a[i],a[j]]
+    if now < 0: # 만약 현재 값이 양수면 오른쪽 포인터를 왼쪽으로 이동
+        i += 1
+    else:       # 현재 값이 음수이면 왼쪽 포인터를 오른쪽으로 이동
+        j -= 1
+print(*ans)
