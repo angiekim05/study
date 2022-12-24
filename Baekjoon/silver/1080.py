3bbeafc69c4f1196b1e43d878decfd6f1210c21d
angiekim05 <70126055+angiekim05@users.noreply.github.com>
2022-12-24 23:34:59 +0900

    그리디

--- Baekjoon/silver/1080.py
+++ Baekjoon/silver/1080.py
@@ -8,14 +8,16 @@ b = [list(map(int,list(input().strip()))) for _ in range(n)]
 def flip(x,y):
     for i in range(3):
         for j in range(3):
+            # 0 <-> 1로 전환 시킴
             a[x+i][y+j] = 1 - a[x+i][y+j]
 def sol():
-    cnt = 0
-    if a == b: # 처음 같은 경우는 바로 0 return
+    if a == b: # 처음에 같은 경우는 바로 0 return
         return 0
+
+    cnt = 0
     for i in range(n-2):
         for j in range(m-2):
-            if a[i][j] != b[i][j]:
+            if a[i][j] != b[i][j]: #만약 서로 다르면 a의 3x3을 전환시킴
                 flip(i,j)
                 cnt += 1
 
