e85fe4d4226450031310d378280beca6c46fd14e
angiekim05 <favor3213@gmail.com>
2022-12-31 23:01:52 +0900

    구현

--- Baekjoon/silver/2563.py
+++ Baekjoon/silver/2563.py
@@ -4,6 +4,7 @@ def makeBlack(x,y):
     for i in range(10):
         for j in range(10):
             # x,y부터 총 10*10 넓이를 훑으면서 방문하지 않은 칸을 세어줌
+            # 색종이가 도화지 밖으로 나가지 않아 걱정하지 않아도 됨
             if not visited[x+i][y+j]: 
                 ans += 1
                 visited[x+i][y+j] = 1
