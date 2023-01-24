30900c51e1fbf8cc1826c21f801755b91c6f09d4
angiekim05 <favor3213@gmail.com>
2023-01-24 17:22:14 +0900

    update saving dataset to save time

--- Paper_Code_Practice/Transformer/data.py
+++ Paper_Code_Practice/Transformer/data.py
@@ -9,13 +9,61 @@ from torch.utils.data import DataLoader
 import time
 start_time = time.time()
 print("Data preprocessing...")
-# train,test,valid = 76125,1603,792
-train_idx, train_src = tokenize_ko_sen("dataset/dataset/korean-english-park.train.ko") # max token length = 129
-valid_idx, valid_src = tokenize_ko_sen("dataset/dataset/korean-english-park.dev.ko") # max token length = 90
-test_idx, test_src = tokenize_ko_sen("dataset/dataset/korean-english-park.test.ko") # max token length = 107
-train_tgt = tokenize_en_sen("dataset/dataset/korean-english-park.train.en", train_idx) # max token length = 96
-valid_tgt = tokenize_en_sen("dataset/dataset/korean-english-park.dev.en", valid_idx) # max token length = 71
-test_tgt = tokenize_en_sen("dataset/dataset/korean-english-park.test.en", test_idx) # max token length = 74
+if not os.path.exists(f'{save_path}dataset'):
+    os.makedirs(f'{save_path}dataset')
+if not os.listdir(f'{save_path}dataset'):
+    # train,test,valid = 76125,1603,792
+    train_idx, train_src = tokenize_ko_sen("dataset/dataset/korean-english-park.train.ko") # max token length = 129
+    valid_idx, valid_src = tokenize_ko_sen("dataset/dataset/korean-english-park.dev.ko") # max token length = 90
+    test_idx, test_src = tokenize_ko_sen("dataset/dataset/korean-english-park.test.ko") # max token length = 107
+    train_tgt = tokenize_en_sen("dataset/dataset/korean-english-park.train.en", train_idx) # max token length = 96
+    valid_tgt = tokenize_en_sen("dataset/dataset/korean-english-park.dev.en", valid_idx) # max token length = 71
+    test_tgt = tokenize_en_sen("dataset/dataset/korean-english-park.test.en", test_idx) # max token length = 74
+
+    # data save
+    with open(f"{save_path}dataset/train_src.txt", "w") as f:
+        for data in train_src:
+            f.write(" ".join(data)+"\n")
+    with open(f"{save_path}dataset/valid_src.txt", "w") as f:
+        for data in valid_src:
+            f.write(" ".join(data)+"\n")
+    with open(f"{save_path}dataset/test_src.txt", "w") as f:
+        for data in test_src:
+            f.write(" ".join(data)+"\n")
+    with open(f"{save_path}dataset/train_tgt.txt", "w") as f:
+        for data in train_tgt:
+            f.write(" ".join(data)+"\n")
+    with open(f"{save_path}dataset/valid_tgt.txt", "w") as f:
+        for data in valid_tgt:
+            f.write(" ".join(data)+"\n")
+    with open(f"{save_path}dataset/test_tgt.txt", "w") as f:
+        for data in test_tgt:
+            f.write(" ".join(data)+"\n")
+else:
+    train_src = []
+    with open("/content/sample_data/train_src.txt", "r") as f:
+        for data in f.readlines():
+            train_src.append(data.strip().split())
+    valid_src = []
+    with open("/content/sample_data/valid_src.txt", "r") as f:
+        for data in f.readlines():
+            valid_src.append(data.strip().split())
+    test_src = []
+    with open("/content/sample_data/test_src.txt", "r") as f:
+        for data in f.readlines():
+            test_src.append(data.strip().split())
+    train_tgt = []
+    with open("/content/sample_data/train_tgt.txt", "r") as f:
+        for data in f.readlines():
+            train_tgt.append(data.strip().split())
+    valid_tgt = []
+    with open("/content/sample_data/valid_tgt.txt", "r") as f:
+        for data in f.readlines():
+            valid_tgt.append(data.strip().split())
+    test_tgt = []
+    with open("/content/sample_data/test_tgt.txt", "r") as f:
+        for data in f.readlines():
+            test_tgt.append(data.strip().split())
 
 # ko vocab: 35857 
 # en vocab: 42217
