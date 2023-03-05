66db15c997491605c8bfb48be988f1079c5b224f
angiekim05 <favor3213@gmail.com>
2023-03-05 21:02:55 +0900

    누적합 | 나머지가 0이 되는 경우의 수는 나머지의 조합으로 구할 수 있음

---
+++ Baekjoon/gold3/10986.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+
+dp = [0]*m # m으로 나눈 나머지 담는 곳
+prefixSum = 0
+for i in range(n):
+    prefixSum += a[i]
+    dp[prefixSum%m] += 1
+
+ans = dp[0] # 나머지가 0이 되는 경우의 수
+for i in range(m):
+    # 같은 크기의 나머지가 되는 prefixSum을 빼서 
+    # 나머지가 0이 되는 nC2 조합을 구해서 더함
+    ans += dp[i] * (dp[i]-1) // 2
+print(ans)
