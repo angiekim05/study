9106dff55bae2207367ba221c805516af1823d0a
angiekim05 <favor3213@gmail.com>
2023-03-10 02:21:39 +0900

    dp

--- Baekjoon/gold3/1099.py
+++ Baekjoon/gold3/1099.py
@@ -1,35 +1,27 @@
 import sys
 input = sys.stdin.readline
 
-def check(w1,w2,l):
+def check(word1,word2,n):
     cnt = 0
-    for i in range(l):
-        if w1[i] != w2[i]:
-            cnt+=1
+    for i in range(n):
+        if word1[i] != word2[i]:
+            cnt += 1
     return cnt
 
 s = " "+input().strip()
 n = int(input())
 words = [input().strip() for _ in range(n)]
-words.sort()
 
-dp = [[-1]*(len(s)) for _ in range(len(s))]
+dp = [[1000]*(len(s)) for _ in range(len(s))]
 dp[0][0] = 0
 
 for i in range(1,len(s)+1):
-    if dp[0][i-1] == -1:
+    if dp[0][i-1] == 1000:
         continue
     for word in words:
         l = len(word)
         if sorted(s[i:i+l]) == sorted(word):
-            if dp[i+l-1][i] == -1:
-                dp[i+l-1][i] = dp[0][i-1]+check(s[i:i+l],word,l)
-            else:
-                dp[i+l-1][i] = min(dp[i+l-1][i],dp[0][i-1]+check(s[i:i+l],word,l))
-            if dp[0][i+l-1] == -1:
-                dp[0][i+l-1] = dp[i+l-1][i]
-            else:
-                dp[0][i+l-1] = min(dp[0][i+l-1],dp[i+l-1][i])
+            dp[i+l-1][i] = min(dp[i+l-1][i],dp[0][i-1]+check(s[i:i+l],word,l))
+            dp[0][i+l-1] = min(dp[0][i+l-1],dp[i+l-1][i])
 
-# print(*dp,sep="\n")
 print(dp[0][-1])
